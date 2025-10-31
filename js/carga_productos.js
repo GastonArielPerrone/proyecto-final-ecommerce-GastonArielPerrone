95a4749ebf1c664ab3d875a520d58e4a806a3a5b
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2025-10-30 23:20:50 -0300

    Products are charged in page "Productos" . In productos.html tags of article for productos are deleted.

--- js/carga_productos.js
+++ js/carga_productos.js
@@ -1,5 +1,21 @@
 export async function cargar_productos(){
     fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
-  .then(productos => console.log(productos));
+  .then(productos => {
+    const contenedor = document.getElementById('productos-container');
+    const mensajeCargando = document.getElementById('mensaje-cargando');
+
+    mensajeCargando.style.display = "none";
+
+    productos.forEach(producto => {
+      const card = document.createElement('article');
+      card.classList.add("article__product");
+      card.innerHTML = `<img src="${producto.image}" alt="${producto.title}" width="150">
+      <h3>${producto.title}</h3>
+      <p>${producto.price}</p>
+      <button data-id="${producto.id}">Añadir al carrito</button>`;
+
+      contenedor.appendChild(card);
+    });
+  });
 }
